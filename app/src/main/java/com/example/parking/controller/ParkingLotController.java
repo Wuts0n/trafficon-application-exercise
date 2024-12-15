e2b1f749b632b3bd5b0cfbeebb1c48c2dfa47cbe
Georg Lang <georg95lang@gmail.com>
2024-12-15 15:32:43 +0100

    remove unused functions

--- app/src/main/java/com/example/parking/controller/ParkingLotController.java
+++ app/src/main/java/com/example/parking/controller/ParkingLotController.java
@@ -1,16 +1,13 @@
 package com.example.parking.controller;
 
-import com.example.exception.UnprocessableEntityException;
 import com.example.parking.model.ParkingLotModel;
 import com.example.parking.model.WrappedLongValue;
 import com.example.parking.service.ParkingLotService;
-import com.example.parking.utility.Parser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,10 +15,6 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/parkingLots")
 public class ParkingLotController {
-
-    private static final String template = "Hello, %s!";
-    private static final String ALL = "*";
-
 
     @Autowired
     private ParkingLotService parkingLotService;
@@ -60,29 +53,4 @@ public class ParkingLotController {
     public void deleteById(@PathVariable Long id) {
         parkingLotService.deleteById(id);
     }
-
-    @GetMapping("/parking")
-    public String getParkingLotById(@RequestParam(value = "ids", defaultValue = ALL) String idsParam) {
-        List<Long> ids = new ArrayList<>();
-        if (idsParam.equals(ALL)) {
-            //TODO get all ids
-            //ids.add(getAllIds());
-            ids.add(5453L);
-            ids.add(42L);
-        } else {
-            try {
-                ids = Parser.stringToLongList(idsParam);
-            } catch (NumberFormatException ex) {
-                throw new UnprocessableEntityException("Some ids are not valid numbers, or you are not using \",\" as delimiter: " + ex.getMessage(), ex);
-            }
-        }
-        //TODO return proper data
-        return ids.toString();
-    }
-
-    @PostMapping("/parking")
-    public String postParkingLot(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format(template, name);
-    }
-
 }
