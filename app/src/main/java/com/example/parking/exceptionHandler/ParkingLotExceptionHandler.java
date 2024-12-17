e28e1b83c717458aedaf907fec2a4512a6fe68e5
Georg Lang <georg95lang@gmail.com>
2024-12-17 13:38:32 +0100

    add EntityNotFoundException handler

--- app/src/main/java/com/example/parking/exceptionHandler/ParkingLotExceptionHandler.java
+++ app/src/main/java/com/example/parking/exceptionHandler/ParkingLotExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.parking.exceptionHandler;
 
 import com.example.exception.UnprocessableEntityException;
+import jakarta.persistence.EntityNotFoundException;
 import org.hibernate.id.IdentifierGenerationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.ErrorResponse;
@@ -11,6 +12,14 @@ import java.net.URI;
 
 @RestControllerAdvice
 public class ParkingLotExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ErrorResponse handleEntityNotFoundException(EntityNotFoundException ex) {
+        return ErrorResponse
+                .builder(ex, HttpStatus.NOT_FOUND, ex.getMessage())
+                .type(URI.create("https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/404"))
+                .build();
+    }
 
     @ExceptionHandler(UnprocessableEntityException.class)
     public ErrorResponse handleUnprocessableEntityException(UnprocessableEntityException ex) {
